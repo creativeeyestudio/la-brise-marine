b2139f5c71cc4adbb14890489df0f7f8b0f4872e
creativeyefr <contact@creative-eye.fr>
2024-12-10 20:59:09 +0100

    Components de page

--- back/types/generated/components.d.ts
+++ back/types/generated/components.d.ts
@@ -14,17 +14,43 @@ export interface CommonCarousel extends Schema.Component {
 export interface CommonGallery extends Schema.Component {
   collectionName: 'components_common_galleries';
   info: {
+    description: '';
     displayName: 'gallery';
+    icon: 'picture';
   };
   attributes: {
     images: Attribute.Media<'images', true>;
   };
 }
 
+export interface CommonHeroscreen extends Schema.Component {
+  collectionName: 'components_common_heroscreens';
+  info: {
+    displayName: 'heroscreen';
+    icon: 'picture';
+  };
+  attributes: {
+    image: Attribute.Media<'images'>;
+  };
+}
+
+export interface CommonHtmlContent extends Schema.Component {
+  collectionName: 'components_common_html_contents';
+  info: {
+    displayName: 'html-content';
+    icon: 'code';
+  };
+  attributes: {
+    code_html: Attribute.Blocks & Attribute.Required;
+  };
+}
+
 export interface CommonParallax extends Schema.Component {
   collectionName: 'components_common_parallaxes';
   info: {
+    description: '';
     displayName: 'parallax';
+    icon: 'picture';
   };
   attributes: {
     image: Attribute.Media<'images'> & Attribute.Required;
@@ -36,19 +62,22 @@ export interface CommonTextDoubleImage extends Schema.Component {
   info: {
     description: '';
     displayName: 'text-double-image';
+    icon: 'stack';
   };
   attributes: {
+    content_image_1: Attribute.Media<'images'> & Attribute.Required;
+    content_image_2: Attribute.Media<'images'> & Attribute.Required;
     content_text: Attribute.Blocks & Attribute.Required;
-    content_title: Attribute.String;
-    image_1: Attribute.Media<'images'> & Attribute.Required;
-    image_2: Attribute.Media<'images'> & Attribute.Required;
+    content_title: Attribute.String & Attribute.Required;
   };
 }
 
 export interface CommonTextImage extends Schema.Component {
   collectionName: 'components_common_text_images';
   info: {
-    displayName: 'Text-image';
+    description: '';
+    displayName: 'text-image';
+    icon: 'stack';
   };
   attributes: {
     content_image: Attribute.Media<'images'>;
@@ -62,6 +91,8 @@ declare module '@strapi/types' {
     export interface Components {
       'common.carousel': CommonCarousel;
       'common.gallery': CommonGallery;
+      'common.heroscreen': CommonHeroscreen;
+      'common.html-content': CommonHtmlContent;
       'common.parallax': CommonParallax;
       'common.text-double-image': CommonTextDoubleImage;
       'common.text-image': CommonTextImage;
