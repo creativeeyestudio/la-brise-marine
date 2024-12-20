1194970447c79d0fea1ef0d0fe83ff97ba2ddea5
tomas-ryan-lee <rifakev@gmail.com>
2024-12-20 18:34:43 +0100

    Création des components

---
+++ front/src/app/_components/layouts/ContentPage.js
@@ -0,0 +1,13 @@
+import Parallax from '../panels/Parallax';
+import TextImage from '../panels/TextImage';
+import TextDoubleImage from '../panels/TextDoubleImage';
+
+export default function ContentPage() {
+    return(
+        <main>
+            <Parallax />
+            <TextImage />
+            <TextDoubleImage />
+        </main>
+    );
+}
---
+++ front/src/app/_components/layouts/Navigation.js
@@ -0,0 +1,9 @@
+export default function Navigation() {
+    return(
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    );
+}
