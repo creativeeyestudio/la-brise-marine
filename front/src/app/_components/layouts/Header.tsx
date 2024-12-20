ca676e01a0d0c8e8309f01b0fee8bb5a0792ff83
tomas-ryan-lee <rifakev@gmail.com>
2024-12-20 19:36:44 +0100

    Header correction

--- front/src/app/_components/layouts/Header.tsx
+++ front/src/app/_components/layouts/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 const Header: React.FC = () => {
     return(
+        <>
         <header></header>
+        <Navigation />
+        </>
     );
 }
 
