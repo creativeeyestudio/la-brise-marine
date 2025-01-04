661d68d41ea0796ae679f5bb4cee91741c90cba6
tomas-ryan-lee <contact@creative-eye.fr>
2025-01-04 18:47:37 +0100

    Popup Nav Creation

--- back/src/plugins/nav-manager/admin/src/pages/HomePage/index.js
+++ back/src/plugins/nav-manager/admin/src/pages/HomePage/index.js
@@ -4,22 +4,59 @@
  *
  */
 
-import React from "react";
+import React, { useState } from "react";
 import MenuList from "../../components/MenuList";
-import { Button, ContentLayout, HeaderLayout } from "@strapi/design-system";
+import {
+  Button,
+  ContentLayout,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  HeaderLayout,
+  TextInput,
+} from "@strapi/design-system";
 import { Plus } from "@strapi/icons";
 
 const HomePage = () => {
+  const [isDialogVisible, setDialogVisible] = useState(false);
+  const handleOpenDialog = () => setDialogVisible(true);
+  const handleCloseDialog = () => setDialogVisible(false);
+
   return (
     <>
       <HeaderLayout
         title="Menus du site"
         subtitle="Gérez les menus de navigation pour votre site."
         navigationAction={<Button variant="tertiary">Retour</Button>}
-        primaryAction={<Button startIcon={<Plus />}>Créer un menu</Button>}
+        primaryAction={
+          <Button startIcon={<Plus />} onClick={handleOpenDialog}>
+            Créer un menu
+          </Button>
+        }
       />
       <ContentLayout>
         <MenuList />
+
+        <Dialog
+          onClose={handleCloseDialog}
+          title={"Créer un nouveau menu"}
+          isOpen={isDialogVisible}
+          id={undefined}
+        >
+          <DialogBody icon={undefined}>
+            <TextInput label="Nom du menu" name="navName" />
+          </DialogBody>
+          <DialogFooter
+            startAction={
+              <Button variant="success-light">Créer</Button>
+            }
+            endAction={
+              <Button onClick={handleCloseDialog} variant="danger-light">
+                Annuler
+              </Button>
+            }
+          />
+        </Dialog>
       </ContentLayout>
     </>
   );
