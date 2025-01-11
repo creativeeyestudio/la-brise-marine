31a00f96e8017efce18ac08bab71b047054f49b3
tomas-ryan-lee <contact@creative-eye.fr>
2025-01-11 13:45:20 +0100

    Navigation dynamique

--- front/src/pages/blog/index.tsx
+++ front/src/pages/blog/index.tsx
@@ -1,82 +1,61 @@
-import React, { useEffect, useState } from "react";
-import Link from "next/link";
-
-import Footer from "@/app/_components/layouts/Footer";
-import Header from "@/app/_components/layouts/Header";
-import { getAllPosts } from "@/pages/api/posts";
-import Image from "next/image";
-import nextConfig from "../../../next.config";
+// Blog.tsx
+import { GetStaticProps } from "next";
+import { getAllPosts } from "../api/posts";
 import Head from "next/head";
+import Link from "next/link";
+import ContentPage from "@/app/_components/layouts/ContentPage";
+import Image from "next/image";
 import PostProps from "@/interfaces/post";
+import { useState, useEffect } from "react";
 
 interface PostsListProps {
   posts: PostProps[];
 }
 
 const Posts: React.FC<PostsListProps> = ({ posts }) => {
-  const [language, setLanguage] = useState<string>('en');
-  const api = nextConfig.apiUrl;
+  const [language, setLanguage] = useState<string>("en");
 
   useEffect(() => {
     // Récupérer la langue depuis l'en-tête X-User-Language
-    const userLanguage = document.documentElement.lang || 'en';
+    const userLanguage = document.documentElement.lang || "en";
     setLanguage(userLanguage);
   }, []);
 
   return (
-    <>
+    <ContentPage>
       <Head>
-        <title>{"Articles"}</title>
-        <meta name="description" content={"Nos articles"} />
-        <meta property="og:title" content={"Articles"} />
-        <meta property="og:description" content={"Nos articles"} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={`${window.location.href}`} />
-        {/* <meta property="og:image" content={imageUrl} />
-        <meta property="og:image:alt" content={imageAlt} /> */}
-        <meta property="og:locale" content={language} />
-        <meta property="og:site_name" content="Nom de votre site" />
+        <title>Articles</title>
+        <meta name="description" content="Nos articles" />
       </Head>
-      <Header />
-      <main>
-        <h1>Nos actualités</h1>
-        <div className="blog__content">
-          {posts.map((post: PostProps) => (
-            <article className="blog__post" key={post.id}>
-              <figure style={{ aspectRatio: '1/1', position: 'relative' }}>
-                <Image 
-                  src={api + post.attributes.main_image.data.attributes.url} 
-                  alt={post.attributes.main_image.data.attributes.alternativeText}
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition="center"
-                  sizes="(max-width: 768px) 100vw, 50vw"
-                />
-              </figure>
 
-              <h2 className="blog__post__name">{post.attributes.title}</h2>
-              <p className="blog__post__date">
-                {new Date(post.attributes.publishedAt).toLocaleDateString(language)}
-              </p>
-              <Link
-                href={`blog/${post.attributes.slug}`}
-                className="blog__post__link"
-              >
-                Lire l&apos;article
-              </Link>
-            </article>
-          ))}
-        </div>
-      </main>
-      <Footer />
-    </>
+      <h1>Nos actualités</h1>
+      <div className="blog__content">
+        {posts.map((post: PostProps) => (
+          <article className="blog__post" key={post.id}>
+            <figure style={{ aspectRatio: "1/1", position: "relative" }}>
+              <Image
+                src={post.attributes.main_image.data.attributes.url}
+                alt={post.attributes.main_image.data.attributes.alternativeText}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                sizes="(max-width: 768px) 100vw, 50vw"
+              />
+            </figure>
+
+            <h2>{post.attributes.title}</h2>
+            <p>{new Date(post.attributes.publishedAt).toLocaleDateString(language)}</p>
+            <Link href={`/blog/${post.attributes.slug}`}>
+              Lire l&apos;article
+            </Link>
+          </article>
+        ))}
+      </div>
+    </ContentPage>
   );
 };
 
-export default Posts;
-
-export async function getStaticProps() {
-  // Récupération des articles
+export const getStaticProps: GetStaticProps = async () => {
   const postsData = await getAllPosts();
   const posts = postsData.data; // Vérifiez que vos données correspondent à cette structure
 
@@ -85,4 +64,6 @@ export async function getStaticProps() {
       posts,
     },
   };
-}
+};
+
+export default Posts;
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,72 +1,46 @@
-import React, { useEffect, useState } from "react";
-import Header from "@/app/_components/layouts/Header";
-import Footer from "@/app/_components/layouts/Footer";
 import { getHomePage } from "./api/pages";
 import Error from "next/error";
 import PageProps from "@/interfaces/page";
 import Head from "next/head";
 import WebPage from "@/app/_components/templates/WebPage";
+import ContentPage from "@/app/_components/layouts/ContentPage";
+import { GetStaticProps } from "next";
 
-const PageHome: React.FC = () => {
-  const [page, setPage] = useState<PageProps | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+interface PageHomeProps {
+  page: PageProps | null;
+  error: string | null;
+}
 
-  useEffect(() => {
-    const fetchPage = async () => {
-      try {
-        setLoading(true);
-        const response = await getHomePage();
-        setPage(response.data[0]);
-      } catch (error) {
-        console.error(error);
-        setError("Erreur lors du chargement de la page");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchPage();
-  }, []);
-
-  if (loading) {
-    return (
-      <>
-        <p>Chargement en cours</p>
-      </>
-    );
-  }
-
+const PageHome: React.FC<PageHomeProps> = ({ page, error }) => {
   if (error) {
-    <>
-      <p>{error}</p>
-    </>;
+    return <Error statusCode={404} />;
   }
 
   if (!page) {
-    return (
-      <>
-        <Error statusCode={404} />
-      </>
-    );
+    return <Error statusCode={404} />;
   }
 
   const blocks = page.attributes.content_page;
 
   return (
-    <>
+    <ContentPage>
       <Head>
         <title>{page.attributes.meta_title}</title>
         <meta name="description" content={page.attributes.meta_desc} />
       </Head>
-
-      <Header />
-
-      <WebPage blocks={blocks}></WebPage>
-      
-      <Footer />
-    </>
+      <WebPage blocks={blocks} />
+    </ContentPage>
   );
+};
+
+// Cette fonction sera exécutée côté serveur pour récupérer les données lors de la génération de la page
+export const getStaticProps: GetStaticProps = async () => {
+  try {
+    const response = await getHomePage();
+    return { props: { page: response.data[0], error: null } };
+  } catch (error) {
+    return { props: { page: null, error: "Erreur lors du chargement de la page" + error } };
+  }
 };
 
 export default PageHome;
