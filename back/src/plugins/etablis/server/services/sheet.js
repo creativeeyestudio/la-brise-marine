88b18fc5aa564976afd2f5b61f085a8c81165a9d
tomas-ryan-lee <contact@creative-eye.fr>
2025-01-12 16:43:14 +0100

    Controller Find

--- back/src/plugins/etablis/server/services/sheet.js
+++ back/src/plugins/etablis/server/services/sheet.js
@@ -15,5 +15,21 @@ module.exports = ({ strapi }) => ({
             strapi.log.error('Erreur dans le service create :', error);
             throw new Error('Échec de la création.');
         }
-    }
+    },
+
+    async find(ctx) {
+        try {
+            // Appel au service pour récupérer l'entité unique
+            const entity = await strapi.plugin('etablis').service('sheet').find();
+        
+            if (!entity) {
+                return ctx.notFound('La fiche n\'existe pas.');
+            }
+        
+            return ctx.send(entity);
+        } catch (error) {
+            strapi.log.error('Erreur lors de la récupération de la fiche :', error);
+            return ctx.internalServerError('Impossible de récupérer la fiche.');
+        }
+    },
 })
