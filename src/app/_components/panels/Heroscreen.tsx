99c9dc4b529edd935023fb2bca16f76b4ee9934b
tomas-ryan-lee <contact@creative-eye.fr>
2025-02-16 19:21:19 +0100

    Heroscreen Swiper

--- src/app/_components/panels/Heroscreen.tsx
+++ src/app/_components/panels/Heroscreen.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import { ImageMultipleProps } from "@/app/interfaces/_image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export interface HeroscreenProps {
   images: ImageMultipleProps;
@@ -8,24 +16,31 @@ export interface HeroscreenProps {
 
 const Heroscreen: React.FC<HeroscreenProps> = (content: HeroscreenProps) => {
   
-  console.log(content);
-  
   return (
-    <div className="hero w-full h-screen">
+    <Swiper 
+      effect="fade"
+      centeredSlides={true}
+      pagination={{ dynamicBullets: true }}
+      loop={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      modules={[EffectFade, Pagination, Autoplay]}
+      className="h-screen"
+    >
       {content.images.data.map((image, index) => (
-        <figure key={index} className="relative h-screen">
+        <SwiperSlide key={index} className="relative">
           <Image
             src={process.env.NEXT_PUBLIC_API_URL + image.attributes.url}
-            alt={image.attributes.alternativeText}
+            alt={image.attributes.alternativeText ?? "Pas de text alt"}
             fill={true}
             objectFit="cover"
-            objectPosition="center"
             priority={true}
-            className="hero_img"
-          />
-        </figure>
+            className="hero_img" />
+        </SwiperSlide>
       ))}    
-    </div>
+    </Swiper>
   );
 };
 
