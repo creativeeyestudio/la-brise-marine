5213d07a0e7196b533b46e6607cd7e37ebe41370
tomas-ryan-lee <contact@creative-eye.fr>
2025-03-08 16:29:21 +0100

    Parallax

--- src/app/_components/panels/Parallax.tsx
+++ src/app/_components/panels/Parallax.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import React from 'react';
 import { ImageProps } from "@/app/interfaces/_image";
 import Ukiyo from "ukiyojs";
 
@@ -9,20 +8,19 @@ interface ParallaxProps {
 }
 
 const Parallax: React.FC<ParallaxProps> = (content: ParallaxProps) => {
-    
     new Ukiyo('.prx_img', {
         speed: content.speed ?? 1.5
     });
 
     return(
         <>
-        <figure className="prx h-[75vh] overflow-hidden relative">
-            <Image
-                className="prx_img"
-                src={process.env.NEXT_PUBLIC_API_URL + content.image.data.attributes.url}
-                alt={content.image.data.attributes.alternativeText}
-                fill={true}/>
-        </figure>
+            <figure className="prx h-[70vh] overflow-hidden relative">
+                <Image
+                    className="prx_img"
+                    src={process.env.NEXT_PUBLIC_API_URL + content.image.data.attributes.url}
+                    alt={content.image.data.attributes.alternativeText}
+                    fill={true}/>
+            </figure>
         </>
     )
 }
