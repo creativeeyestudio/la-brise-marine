b1f71b1868842bf3657bfd82fbd1c4c8214b456d
tomas-ryan-lee <contact@creative-eye.fr>
2025-03-12 23:30:23 +0100

    Remove smooth scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,45 +13,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     const container: HTMLElement | null = document.getElementById('content');
   
     if (container) {
-      const scrollbar = Scrollbar.init(container, {
-        damping: .05,
-        thumbMinSize: 20,
-        renderByPixels: false,
-        alwaysShowTracks: true,
-        continuousScrolling: true
-      });
-      const aosAttr = document.querySelectorAll('[data-aos]');
-  
-      aosAttr.forEach((el) => {
-        scrollbar.addListener(() => {
-          (el as HTMLElement).classList.toggle('aos-animate', scrollbar.isVisible(el as HTMLElement));
-        });
-      });
-
-      if (window.innerWidth >= 1280) {
-        const elements = [
-          { selector: '.header', offset: 0 },
-          { selector: '.menu--primary', offset: 96 },
-          { selector: '.menu--secondary', offset: 96 }
-        ];
-      
-        const domElements = elements
-          .map(({ selector, offset }) => ({
-            el: document.querySelector(selector) as HTMLElement | null,
-            offset
-          }))
-          .filter(({ el }) => el !== null); // Filtrer les éléments qui existent
-      
-        if (domElements.length > 0) {
-          scrollbar.addListener(({ offset }) => {
-            domElements.forEach(({ el, offset: elOffset }) => {
-              if (el) el.style.top = `${offset.y + elOffset}px`;
-            });
-          });
-        }
-      }
-      
-  
       AOS.init({
         duration: 1500,
         delay: 500,
@@ -60,14 +21,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       });
   
       return () => {
-        scrollbar.destroy(); // Nettoyage du Scrollbar à la suppression du composant
         AOS.refreshHard(); // Rafraîchissement plus complet d'AOS
       };
     }
   }, []);  
 
   return (
-    <div id='content' className={`overflow-x-hidden lg:h-lvh font-body ${poppins.variable} ${raleway.variable} ${roboto.variable}`}>
+    <div id='content' className={`overflow-x-hidden font-body ${poppins.variable} ${raleway.variable} ${roboto.variable}`}>
       <Component {...pageProps} />
     </div>
   );
