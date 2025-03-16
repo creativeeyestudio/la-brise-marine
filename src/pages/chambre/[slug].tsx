543ce4e2a2d93b5140ca4bc8f07ddf4d73fe51d9
Creative Eye Studio <contact@creative-eye.fr>
2025-03-16 17:40:44 +0100

    Page Chambre - Corrections

--- src/pages/chambre/[slug].tsx
+++ src/pages/chambre/[slug].tsx
@@ -2,13 +2,31 @@ import ContentPage from "@/app/_components/layouts/ContentPage";
 import Image from "next/image";
 import Head from "next/head";
 import ButtonLink from "@/app/_components/buttonLink";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import _ from 'lodash';
 
 const RoomPage = () => {
+  const [room, setRoom] = useState(null);
+  
+  useEffect(() => {
+    const fetchRoom = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_ROOMS_MANAGER}/1`);
+        setRoom(response.data);
+      } catch (error) {
+        console.error("Error fetching room:", error);
+      }
+    };
+
+    fetchRoom();
+  }, []);
+
   return (
     <>
       <ContentPage secondary_page={false}>
         <Head>
-          <title>Nom de la chambre</title>
+          <title>{room?.room_name}</title>
           <meta name="description" content="" />
           <meta http-equiv="X-UA-Compatible" content="ie=edge" />
           <meta
@@ -18,29 +36,18 @@ const RoomPage = () => {
         </Head>
 
         {/* Heroscreen */}
-        <figure className="relative aspect-[4/3] md:aspect-video">
-          <Image src="" alt="Chambre" fill={true} objectFit="cover"></Image>
+        <figure className="relative aspect-[4/3] md:aspect-video lg:aspect-[inherit] lg:h-dvh">
+          <Image src={`/images/chambres/` + _.kebabCase(room?.room_name) + '.jpg'} alt="Chambre" fill={true} objectFit="cover"></Image>
         </figure>
 
         {/* Intro */}
         <section className="px-xs py-xs sm:px-sm sm:py-sm md:px-md md:py-md lg:px-lg lg:py-lg xl:grid xl:grid-cols-3 xl:gap-lg">
           <div className="xl:col-span-2">
-            <h1 className="text-3xl">Nom de la chambre</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
-              ea soluta neque necessitatibus, earum magnam sapiente blanditiis
-              odit error animi, officia deleniti tempora ipsam magni eum ipsa,
-              sed sunt ipsum.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid
-              ea soluta neque necessitatibus, earum magnam sapiente blanditiis
-              odit error animi, officia deleniti tempora ipsam magni eum ipsa,
-              sed sunt ipsum.
-            </p>
+            <h1 className="text-3xl">{room?.room_name}</h1>
+            <p>{room?.room_desc}</p>
 
             <div className="mt-4 flex flex-col gap-4 xl:flex-row xl:justify-between xl:mt-md">
-              <span>0,00 €</span>
+              <span>{room?.room_price} €</span>
               <ButtonLink
                 primary={true}
                 label={"Réserver une chambre"}
@@ -49,12 +56,15 @@ const RoomPage = () => {
               ></ButtonLink>
             </div>
           </div>
-          <div className="bg-tertiary mt-8 px-xs py-xs sm:px-sm sm:py-sm xl:col-start-3 xl:h-max">
+
+          <div className="bg-tertiary mt-8 px-xs py-xs sm:px-sm sm:py-sm xl:col-start-3 xl:h-max xl:mt-0">
             <p className="uppercase">
               <strong>Nos services</strong>
             </p>
-            <ul className="list-disc ms-4">
-              <li>Liste des prestations</li>
+            <ul className="list-disc ms-8 leading-7">
+              {room?.room_services.map((service: string, index: number) => (
+                <li key={index}>{service}</li>
+              ))}
             </ul>
           </div>
         </section>
